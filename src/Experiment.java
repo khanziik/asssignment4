2c45aa7976c6d956565bdf08e3e1a1e78d629a73
Daulet <253911@astanait.edu.kz>
2026-05-10 01:53:24 +0500

    a4

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,2 +1,45 @@
 public class Experiment {
+    //run traversals
+    public void runTraversals(Graph g){
+        long startBFS = System.nanoTime();
+        g.bfs(0);
+        long endBFS = System.nanoTime();
+        long bfsTime = endBFS - startBFS;
+        long startDFS = System.nanoTime();
+        g.dfs(0);
+        long endDFS = System.nanoTime();
+        long dfsTime = endDFS - startDFS;
+
+        System.out.println("BFS Time: " + bfsTime + " ns");
+        System.out.println("DFS Time: " + dfsTime + " ns");
+    }
+    //create graph
+    public Graph createGraph(int size){
+        Graph g = new Graph();
+        //add verticies
+        for(int i = 0; i < size -1; i++){
+            g.addVertex(new Vertex(i));
+        }
+        //add edges
+        for (int i = 0; i< size - 1; i++){
+            g.addEdge(i. i + 1);
+        }
+        return g;
+    }
+    //multiple tests
+    public void runMultipleTests(){
+        int[] sizes = {10. 30, 100};
+        for(int size : sizes){
+            System.out.println("\nGraph size: " + size);
+            Graph g = createGraph(size);
+            if(size == 10){
+                g.printGraph();
+            }
+            runTraversals(g);
+        }
+    }
+    //print
+    public void printResults(){
+        System.out.println("Experiments completed");
+    }
 }
