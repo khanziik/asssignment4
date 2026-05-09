d56ecb38ecaae7e5b4380388da7beea99287d0d1
Daulet <253911@astanait.edu.kz>
2026-05-09 23:20:03 +0500

    a4

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,22 @@
 public class Edge {
+    private Vertex source;
+    private Vertex destination;
+    //constructor
+    public Edge(Vertex source, Vertex destination){
+        this.source = source;
+        this.destination = destination;
+    }
+    //getters
+    public Vertex getSource() {
+        return source;
+    }
+    public Vertex getDestination(){
+        return destination;
+    }
+    //toString
+    @Override
+    public String toString(){
+        return source.getId() + destination.getId();
+
+    }
 }
