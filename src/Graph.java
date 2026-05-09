d164ff8811cafffeaad751c6372b841bd7af955d
Daulet <253911@astanait.edu.kz>
2026-05-09 23:56:13 +0500

    a4

--- src/Graph.java
+++ src/Graph.java
@@ -1,2 +1,75 @@
+import java.sql.Array;
+import java.util.*;
 public class Graph {
+    //adjacency list
+    private Map<Integer, List<Integer>> adjacencyList;
+
+    //constructor
+    public Graph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    //add vertex
+    public void addVertex(Vertex v) {
+        adjacencyList.putIfAbsent(v.getId(), new ArrayList<>());
+    }
+
+    //add edge
+    public void addEdge(int from, int to) {
+        adjacencyList.putIfAbsent(from, new ArrayList<>());
+        adjacencyList.putIfAbsent(to, new ArrayList<>());
+
+        adjacencyList.get(from).add(to);
+        //undirected graph
+        adjacencyList.get(to).add(from);
+    }
+
+    //print graph
+    public void printGraph() {
+        for (int vertex : adjacencyList.keySet()) {
+            System.out.print(vertex);
+            for (int neighbor : adjacencyList.get(vertex)) {
+                System.out.print(neighbor);
+            }
+            System.out.println();
+        }
+    }
+
+    //bfs
+    public void bfs(int start) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        visited.add(start);
+        queue.add(start);
+        System.out.print("BFS:");
+
+        while (!queue.isEmpty()){
+            int current = queue.poll();
+            System.out.print(current + " ");
+            for(int neighbor : adjacencyList.get(current)){
+                if (!visited.contains(neighbor)){
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+    //DFS
+    public void dfs(int start) {
+        Set<Integer> visited = new HashSet<>();
+        System.out.print("DFS: ");
+        dfsHelper(start, visited);
+        System.out.println();
+    }
+    //helper methode
+    private void dfsHelper(int current, Set<Integer> visited ){
+        visited.add(current);
+        System.out.print(current)
+                for(int neighbor : adjacencyList.get(current)){
+                    if (!visited.contains(neighbor)){
+                        dfsHelper(neighbor, visited);
+                    }
+                }
+    }
 }
